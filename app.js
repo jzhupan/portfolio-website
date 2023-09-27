afedc13686a5ffff2ad5536a91ff51494e44f854
jzhupan <jzhupan@gmail.com>
2023-09-27 19:32:18 -0400

    Added toggle theme

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add('show')
-        } else {
-            entry.target.classList.remove('show')
-        }
+document.addEventListener("DOMContentLoaded", () => {
+    const themeButton = document.getElementById("theme-button");
+
+    themeButton.addEventListener("click", () => {
+        const currentTheme = document.body.getAttribute("data-theme");
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        document.body.setAttribute("data-theme", newTheme);
+    });
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            //console.log(entry)
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show')
+            } else {
+                entry.target.classList.remove('show')
+            }
+        })
     })
-})
+    
+    
+    const hiddenElements = document.querySelectorAll('.hidden')
+    hiddenElements.forEach((el) => observer.observe(el))
 
 
-const hiddenElements = document.querySelectorAll('.hidden')
-hiddenElements.forEach((el) => observer.observe(el))
+});
