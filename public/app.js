fcff726e36134f11c98b13ce8a4f4aec85c8fdbd
jzhupan <jzhupan@gmail.com>
2023-10-13 16:06:06 -0400

    Organized directory and redeployed

---
+++ public/app.js
@@ -0,0 +1,61 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const themeButton = document.getElementById("theme-button");
+    const icon = document.getElementById('iconImage')
+    
+
+    themeButton.addEventListener("click", () => {
+
+        const currentTheme = document.body.getAttribute("data-theme");
+        const newTheme = currentTheme === "light" ? "dark" : "light";
+        document.body.setAttribute("data-theme", newTheme);
+
+            if(newTheme === "light"){
+                icon.src = './images/logo-icons/bxs-moon.png'
+            } else if(newTheme === "dark"){
+                icon.src = './images/logo-icons/bxs-sun.png'
+            }
+
+
+    });
+   
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            //console.log(entry)
+            if(entry.isIntersecting) {
+                entry.target.classList.add('show')
+            } else {
+                entry.target.classList.remove('show')
+            }
+        })
+    })
+    
+    
+    const hiddenElements = document.querySelectorAll('.hidden')
+    hiddenElements.forEach((el) => observer.observe(el))
+
+    
+});
+
+function navFunction(){
+    let x = document.getElementById('nav-bar');
+    if(x.className === 'topnav'){
+        x.className += ' responsive';
+    } else {
+        x.className = 'topnav';
+    }
+}
+
+
+//Browser Icon
+let link = document.querySelector("link[rel~='icon']");
+if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.head.appendChild(link);
+}
+link.href = './images/logo-icons/black-icon-transparentbg.png'
+
+
+
