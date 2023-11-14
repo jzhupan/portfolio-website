94f9cd6a77ec4d3f813c66ea183c3dab7794eb62
jzhupan <jzhupan@gmail.com>
2023-11-14 14:55:31 -0500

    There was a bug where the clickable stuff were not working, fixed.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 particlesJS.load("particles-js", "/particlesjs-config.json", function () {
-  console.log("callback - app.js config loaded");
+  //console.log("callback - app.js config loaded");
 });
 
 document.addEventListener("DOMContentLoaded", () => {
